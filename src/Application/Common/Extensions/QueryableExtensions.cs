5400767a047af7ae352b9f49f9ee93660309edff
vuphamhocvienmatma <72012725+vuphamhocvienmatma@users.noreply.github.com>
2025-09-02 18:04:25 +0700

    Update QueryableExtensions.cs

--- src/Application/Common/Extensions/QueryableExtensions.cs
+++ src/Application/Common/Extensions/QueryableExtensions.cs
@@ -4,13 +4,7 @@ namespace Application.Common.Extensions;
 
 public static class QueryableExtensions
 {
-    /// <summary>
-    /// Áp dụng phân trang cho một IQueryable.
-    /// </summary>
-    /// <param name="source">Nguồn dữ liệu IQueryable.</param>
-    /// <param name="pageNumber">Số trang hiện tại (bắt đầu từ 1).</param>
-    /// <param name="pageSize">Số lượng mục trên mỗi trang.</param>
-    /// <returns>Một PagedResult chứa dữ liệu của trang hiện tại và thông tin phân trang.</returns>
+   
     public static async Task<PagedResult<T>> ToPagedResultAsync<T>(
         this IQueryable<T> source,
         int pageNumber,
