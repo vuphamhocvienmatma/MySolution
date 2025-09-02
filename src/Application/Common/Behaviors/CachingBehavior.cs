5fb6e951e824d7e8e420b7d16fd482e210cefbc7
vuphamhocvienmatma <72012725+vuphamhocvienmatma@users.noreply.github.com>
2025-09-02 18:03:58 +0700

    Update CachingBehavior.cs

--- src/Application/Common/Behaviors/CachingBehavior.cs
+++ src/Application/Common/Behaviors/CachingBehavior.cs
@@ -3,9 +3,9 @@
 namespace Application.Common.Behaviors;
 
 public class CachingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
-    where TRequest : ICacheableQuery<TResponse> // Giữ nguyên ICacheableQuery interface
+    where TRequest : ICacheableQuery<TResponse> 
 {
-    private readonly ICacheService _cacheService; // Dùng ICacheService đa tầng
+    private readonly ICacheService _cacheService;
     private readonly ILogger<CachingBehavior<TRequest, TResponse>> _logger;
 
     public CachingBehavior(ICacheService cacheService, ILogger<CachingBehavior<TRequest, TResponse>> logger)
